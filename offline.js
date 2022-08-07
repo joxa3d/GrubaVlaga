7fc554119918f474edaa5d779a537009b871f3d4
Vladimir <joxa3d@gmail.com>
2022-08-07 08:49:28 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,17 @@
+﻿{
+	"version": 1659854440,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/lookto-sheet0.png",
+		"images/format-sheet0.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
